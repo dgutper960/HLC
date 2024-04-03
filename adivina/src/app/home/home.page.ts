736d973bfae9d7df5b88491c1b3540677e9d3b65
dgutper960 <dgutper960@outlook.es>
2024-04-03 14:41:04 +0200

    finaliza app funciona correctamente

--- adivina/src/app/home/home.page.ts
+++ adivina/src/app/home/home.page.ts
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  num: number = 0; // Esta variable la vamos a inicializar con el input del html mediante [(ngModel)]="num"
+  numSecret: number = Math.round(Math.random()*100+1);
+  respuesta: string = "¿La suerte te acompaña?"; // esta variable la vamos a mostrar en el html
+
   constructor() {}
+// Función que compara el número introducido por el usuario con numSecret
+  comprobarNumero(){
+    // comprobamos el el número tiene valor diferente a 0
+    if(this.num != 0){
+      // Comparamos el valor de los números
+      if(this.numSecret < this.num){
+        this.respuesta = "El número que buscas es menor al intoducido"
+      }else if(this.numSecret > this.num){
+        this.respuesta = "El número que buscas es mayor al intoducido"
+      }else{
+        this.respuesta = "¡¡ENHORABUENA!! Acertaste el número!!"
+      }
+    }
+
+  }
 
 }
