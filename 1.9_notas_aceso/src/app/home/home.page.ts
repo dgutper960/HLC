d591d4185a560afd0295a4c40879f9e4fdc09a60
dgutper960 <dgutper960@outlook.es>
2024-04-04 11:55:27 +0200

    implementa html y plantea ts

--- 1.9_notas_aceso/src/app/home/home.page.ts
+++ 1.9_notas_aceso/src/app/home/home.page.ts
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  // Definimos las varibles para las notas
+  lengua: number = 0; // si no están inicializadas nos da error
+  comentario: number = 0;
+  ingles: number = 0;
+  especifica1: number = 0;
+  especifica2: number = 0;
+
+  // Definimos las variables para las medias y nota final
+  medGeneral: number = 0;
+  medEspecifica: number = 0;
+  notaFinal: number = 0;
+
   constructor() {}
+
+  // definimos el método que calcula la nota final
+  calcNotaFinal(general: number, especifica: number){
+    return (general+especifica)/2;
+  }
 
 }
