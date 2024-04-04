d1e81ca07cb0241281c5f72d017c759bb209a15c
dgutper960 <dgutper960@outlook.es>
2024-04-04 11:15:22 +0200

    inicio notas acceso

---
+++ 1.9_notas_aceso/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '1.9_notas_aceso',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
