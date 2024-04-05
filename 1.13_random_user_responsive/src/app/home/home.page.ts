2dbaeacb86783619540532a764164eb5f2413999
dgutper960 <dgutper960@outlook.es>
2024-04-05 13:53:47 +0200

    app responsive finalizada

--- 1.13_random_user_responsive/src/app/home/home.page.ts
+++ 1.13_random_user_responsive/src/app/home/home.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+// Define una interfaz para la respuesta de la API
+interface ApiResponse {
+ results: any[]; // Define el tipo de los resultados aquí
+}
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+ selector: 'app-home',
+ templateUrl: 'home.page.html',
+ styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+ // Almacenamos array obtenido en la API random.me (20 perfiles)
+ // indicamos que el tipo de dato es un objeto
+ users$: Observable<any[]>;
 
+ constructor(private httpClient: HttpClient) {
+    // Utilizamos el operador map para extraer la propiedad 'results' de la respuesta
+    this.users$ = this.httpClient.get<ApiResponse>('https://randomuser.me/api/?results=20').pipe(
+      map(data => data.results)
+    );
+ }
 }
