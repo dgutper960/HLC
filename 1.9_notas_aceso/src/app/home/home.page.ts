1f4919541239238f993eeb55e02043a8982a70c3
dgutper960 <dgutper960@outlook.es>
2024-04-05 10:36:17 +0200

    depuración

--- 1.9_notas_aceso/src/app/home/home.page.ts
+++ 1.9_notas_aceso/src/app/home/home.page.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   // Definimos las varibles para las notas
   lengua: number = 0; // si no están inicializadas nos da error
@@ -19,8 +19,19 @@ export class HomePage {
   medGeneral: number = 0;
   medEspecifica: number = 0;
   notaFinal: number = 0;
+  mensaje: string = "";
 
   constructor() {}
+
+  ngOnInit(){
+    this.lengua = 0;
+    this.comentario = 0;
+    this.ingles = 0;
+    this.especifica1 = 0;
+    this.especifica2 = 0;
+    this.notaFinal = 0;
+    this.mensaje = "";
+  }
 
   // definimos el método que calcula la nota final
   calcNotaFinal(general: number, especifica: number){
@@ -36,9 +47,15 @@ export class HomePage {
     // si ambas estan dentro del rango indicado llamamos calculamos la nota final
     if(this.medEspecifica >= 4 && this.medGeneral >= 4){
       this.notaFinal = this.calcNotaFinal(this.medGeneral, this.medEspecifica);
+    }else if(this.medEspecifica < 4){
+      this.mensaje = "La nota media en uno de los bloques es insuficiente";
+    }else if(this.medGeneral < 4){
+      this.mensaje = "La nota media en uno de los bloques es insuficiente";
     }
 
     console.log(this.notaFinal);
+    console.log(this.medEspecifica);
+    console.log(this.medGeneral);
   }
 
 }
