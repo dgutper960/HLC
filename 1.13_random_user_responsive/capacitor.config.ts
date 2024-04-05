985b4bd9079597909ca8de69ed2c78ea1472cc73
dgutper960 <dgutper960@outlook.es>
2024-04-05 11:49:19 +0200

    inici actividad 1.13

---
+++ 1.13_random_user_responsive/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '1.13_random_user_responsive',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
